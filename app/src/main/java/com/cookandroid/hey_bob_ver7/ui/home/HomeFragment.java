bd4a06ef9c44f088ae6ec0d4326872e9ae4a2127
simfrog <ism0705@naver.com>
2020-12-17 00:12:45 +0900

    Hey_bob_listView

--- app/src/main/java/com/cookandroid/hey_bob_ver7/ui/home/HomeFragment.java
+++ app/src/main/java/com/cookandroid/hey_bob_ver7/ui/home/HomeFragment.java
@@ -5,31 +5,68 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.R.id.*;
 
+import android.graphics.drawable.Drawable;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.cookandroid.hey_bob_ver7.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends ListFragment {
 
-    private HomeViewModel homeViewModel;
+    ListViewAdapter adapter ;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Adapter 생성 및 Adapter 지정.
+        adapter = new ListViewAdapter() ;
+        setListAdapter(adapter) ;
+
+
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_macdonald),
+                "맥도날드", "함께배달주문/+1인/아이파크 정문") ;
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_costco),
+                "코스트코 도넛", "함께구매/+1인/동작구 상도 1길") ;
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_yeopgittoekbokki),
+                "엽기떡볶이", "함께배달주문/+4인/숭실대 레지던스홀") ;
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_gsshop),
+                "GS 홈쇼핑 여성 터틀넥", "함께구매/+4인/노량진") ;
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_homeplus),
+                "홈플러스 계란", "함께구매/+2인/이수") ;
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_yugane),
+                "유가네 닭갈비", "함께배달주문/+2인/숭실대 레지던스홀") ;
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_ssamrice),
+                "백종원의 원조쌈밥집", "함께배달주문/+3인/자이 아파트") ;
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_dumpling),
+                "명인만두", "함께배달주문/+1인/푸르지오 정문") ;
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
+
+
+    @Override
+    public void onListItemClick (ListView l, View v, int position, long id) {
+        // get TextView's Text.
+        ListViewItem item = (ListViewItem) l.getItemAtPosition(position) ;
+
+        String titleStr = item.getTitle() ;
+        String descStr = item.getDesc() ;
+        Drawable iconDrawable = item.getIcon() ;
+
+    }
+
+    public void addItem(Drawable icon, String title, String desc) {
+        adapter.addItem(icon, title, desc) ;
+    }
+
 }
